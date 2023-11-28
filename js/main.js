e40a1dbba8a7c0700b67622de4f3a0beb44eebff
gabox2002 <gabox2002@gmail.com>
2023-11-27 22:12:58 -0300

    adaptacion y mejoras del ejercicio

--- js/main.js
+++ js/main.js
@@ -1,96 +1,159 @@
-// 1.
+// 1. Leer el titulo
 const titleText = document.title;
 console.log(titleText);
 
-// 3.
-document.addEventListener('DOMContentLoaded', function () {
-var integrante1 = obtenerNombreCompleto(1);
-var integrante2 = obtenerNombreCompleto(2);
+// 3. Leer datos y mostrar en consola
+document.addEventListener("DOMContentLoaded", function () {
+    console.log(`-----`);
+    for (let i = 1; i <= 2; i++) {
+        const integrante = obtenerNombreCompleto(i);
+        console.log(`Integrante ${i}: "${integrante.primerNombre} ${ integrante.segundoNombre ? integrante.segundoNombre.concat(" ") : "" }${integrante.primerApellido} ${integrante.segundoApellido}"`);
+    }
+    console.log("-----");
 
-console.log('-----');
-console.log(`Integrante 1: "${integrante1.primerNombre} ${integrante1.segundoNombre ? integrante1.segundoNombre + ' ' : ''}${integrante1.primerApellido} ${integrante1.segundoApellido}"`);
-console.log(`Integrante 2: "${integrante2.primerNombre} ${integrante2.segundoNombre ? integrante2.segundoNombre + ' ' : ''}${integrante2.primerApellido} ${integrante2.segundoApellido}"`);
-console.log('-----');
+    // 4. Comparar nombres y apellidos
+    const integrante1 = obtenerNombreCompleto(1);
+    const integrante2 = obtenerNombreCompleto(2);
 
-// 4.
-var coincidenciasNombres = verificarCoincidencias(integrante1, integrante2, 'Nombre');
+    const coincidenciasNombres = verificarCoincidencias( integrante1, integrante2, "Nombre");
 
-if (confirm('¿Desea comparar apellidos?')) {
-    verificarCoincidencias(integrante1, integrante2, 'Apellido');
-} else {
-    console.log('No se compararán apellidos.');
-}
+    if (confirm("¿Desea comparar apellidos?")) {
+        verificarCoincidencias(integrante1, integrante2, "Apellido");
+    } else {
+        console.log("No se compararán apellidos.");
+    }
 });
 
 function verificarCoincidencias(integrante1, integrante2, tipo) {
-var nombres1 = obtenerNombres(integrante1, tipo);
-var nombres2 = obtenerNombres(integrante2, tipo);
+    const nombres1 = obtenerNombres(integrante1, tipo);
+    const nombres2 = obtenerNombres(integrante2, tipo);
 
-var coincidencias = nombres1.filter(function (nombre) {
-    return nombres2.includes(nombre);
-});
+    const coincidencias = encontrarCoincidencias(nombres1, nombres2);
 
-if (coincidencias.length > 0) {
-    console.log('Hubo coincidencias en', tipo + 's:', coincidencias);
+    if (coincidencias.length > 0) {
+        console.log(`Hubo coincidencias en ${tipo}s:`, coincidencias);
 
-    // Solicitar color para destacar coincidencias
-    var color = prompt('Hubo coincidencias en ' + tipo.toLowerCase() + 's. Ingrese un color para destacarlas:');
+        // Verificar coincidencias en nombres y apellidos
+        const coincidenciasNombres = encontrarCoincidencias(
+            obtenerNombres(integrante1, "Nombre"),
+            obtenerNombres(integrante2, "Nombre")
+        );
+        const coincidenciasApellidos = encontrarCoincidencias(
+            obtenerNombres(integrante1, "Apellido"),
+            obtenerNombres(integrante2, "Apellido")
+        );
 
-    // Aplicar estilos a las coincidencias
-    aplicarEstilos(integrante1, tipo, coincidencias, color);
-    aplicarEstilos(integrante2, tipo, coincidencias, color);
-} else {
-    console.log('No hubo coincidencias en', tipo + 's.');
+        if ( coincidenciasNombres.length > 0 && coincidenciasApellidos.length > 0) {
+            // Solicitar color para destacar coincidencias
+            const color = prompt(
+                `Hubo coincidencias en ${tipo.toLowerCase()}s. Ingrese un color para destacarlas:`
+            );
+
+            // Aplicar estilos a las coincidencias
+            aplicarEstilos(integrante1, tipo, coincidencias, color);
+            aplicarEstilos(integrante2, tipo, coincidencias, color);
+        } 
+    } else {
+        console.log(`No hubo coincidencias en ${tipo}s.`);
+    }
 }
+
+function encontrarCoincidencias(array1, array2) {
+    const coincidencias = [];
+
+    for (let i = 0; i < array1.length; i++) {
+        const valor = array1[i];
+
+        // Verificar si el valor está presente en array2
+        for (let j = 0; j < array2.length; j++) {
+            if (valor === array2[j]) {
+                coincidencias[coincidencias.length] = valor;
+                break;  // Salir del bucle interno una vez que se encuentra la coincidencia
+            }
+        }
+    }
+
+    return coincidencias;
 }
 
 function obtenerNombres(integrante, tipo) {
-var nombres = [];
-if (tipo === 'Nombre') {
-    var primerNombre = integrante.primerNombre.toLowerCase();
-    var segundoNombre = integrante.segundoNombre ? integrante.segundoNombre.toLowerCase() : null;
-    nombres.push(primerNombre);
-    if (segundoNombre) {
-        nombres.push(segundoNombre);
+    let nombres = [];
+    if (tipo === "Nombre") {
+        const primerNombre = integrante.primerNombre.toLowerCase();
+        const segundoNombre = integrante.segundoNombre ? integrante.segundoNombre.toLowerCase(): null;
+        
+        nombres = nombres.concat(primerNombre);
+        if (segundoNombre) {
+            nombres = nombres.concat(segundoNombre);
+        }
+    } else if (tipo === "Apellido") {
+        const primerApellido = integrante.primerApellido.toLowerCase();
+        const segundoApellido = integrante.segundoApellido ? integrante.segundoApellido.toLowerCase(): null; 
+        
+        nombres = nombres.concat(primerApellido);
+        
+        if (segundoApellido) {
+            nombres = nombres.concat(segundoApellido);
+        }
     }
-} else if (tipo === 'Apellido') {
-    var primerApellido = integrante.primerApellido.toLowerCase();
-    var segundoApellido = integrante.segundoApellido ? integrante.segundoApellido.toLowerCase() : null;
-    nombres.push(primerApellido);
-    if (segundoApellido) {
-        nombres.push(segundoApellido);
-    }
-}
-return nombres;
+    return nombres;
 }
 
 function aplicarEstilos(integrante, tipo, coincidencias, color) {
-var selector = `h2:nth-of-type(${integrante.numero}) + dl`;
+    const selector = document.querySelectorAll(`.integrante${numeroIntegrante}`);
 
-coincidencias.forEach(function (coincidencia) {
-    var elementos = document.querySelectorAll(`${selector} dd`);
+    for (let i = 0; i < coincidencias.length; i++) {
+        const coincidencia = coincidencias[i];
+        const elementos = document.querySelectorAll(`${selector} dd`);
 
-    elementos.forEach(function (elemento) {
-        if (elemento.textContent.trim().toLowerCase() === coincidencia.toLowerCase()) {
-            elemento.style.color = color;
+        for (let j = 0; j < elementos.length; j++) {
+            const elemento = elementos[j];
+
+            if (elemento.innerHTML.trim().toLowerCase() === coincidencia.toLowerCase()) {
+                elemento.style.color = color;
+            }
         }
-    });
-});
+    }
 }
 
 function obtenerNombreCompleto(numeroIntegrante) {
-var primerNombre = document.querySelector(`h2:nth-of-type(${numeroIntegrante}) + dl dt:first-of-type + dd`).textContent.trim();
-var segundoNombre = document.querySelector(`h2:nth-of-type(${numeroIntegrante}) + dl dt:nth-of-type(2) + dd`).textContent.trim();
-var primerApellido = document.querySelector(`h2:nth-of-type(${numeroIntegrante}) + dl dt:nth-of-type(3) + dd`).textContent.trim().toUpperCase();
-var segundoApellido = document.querySelector(`h2:nth-of-type(${numeroIntegrante}) + dl dt:nth-of-type(4) + dd`).textContent.trim().toUpperCase();
+    const integrantes = document.querySelectorAll(`.integrante${numeroIntegrante}`);
 
-var nombreCompleto = {
-    numero: numeroIntegrante,
-    primerNombre: primerNombre,
-    segundoNombre: segundoNombre,
-    primerApellido: primerApellido,
-    segundoApellido: segundoApellido
-};
+    let nombreCompleto = {
+        numero: numeroIntegrante,
+        primerNombre: "",
+        segundoNombre: "",
+        primerApellido: "",
+        segundoApellido: "",
+    };
 
-return nombreCompleto;
+    for (let i = 0; i < integrantes.length; i++) {
+        const dataElement = integrantes[i].children;
+
+        for (let index = 0; index < dataElement.length; index++) {
+            const dtElement = dataElement[index];
+
+            if (dtElement.tagName === "DT") {
+                const dataType = dtElement.innerText.trim().toLowerCase();
+                const ddElement = dataElement[index + 1]; // Obtener el siguiente elemento directamente
+
+                switch (dataType) {
+                    case "primer nombre":
+                        nombreCompleto.primerNombre = ddElement.innerText.trim();
+                        break;
+                    case "segundo nombre":
+                        nombreCompleto.segundoNombre = ddElement.innerText.trim();
+                        break;
+                    case "primer apellido":
+                        nombreCompleto.primerApellido = ddElement.innerText.trim().toUpperCase();
+                        break;
+                    case "segundo apellido":
+                        nombreCompleto.segundoApellido = ddElement.innerText.trim().toUpperCase();
+                        break;
+                }
+            }
+        }
+    }
+
+    return nombreCompleto;
 }
